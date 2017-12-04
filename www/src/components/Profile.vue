f32e566bc7fe01ddca5452e3726c10e1e5b67789
Ian Bradley <ianbradleydebate@gmail.com>
2017-12-04 16:29:13 -0700

    profile

--- www/src/components/Profile.vue
+++ www/src/components/Profile.vue
@@ -4,10 +4,16 @@
             <h2>Your Profile</h2>
         </div>
         <div class="panel-body">
-            <h3>Name: {{user.name}}</h3>
-            <h3>Email: {{user.email}}</h3>
-            <h3>Username: {{user.username}}</h3>
-            <h3>Profile picture: {{user.image}}</h3>
+            <div v-if="showProfile">
+                <span class="glyphicon glyphicon-pencil pull-right" @click="showProfile"></span>
+                <h3>Name: {{user.name}}</h3>
+                <h3>Email: {{user.email}}</h3>
+                <h3>Username: {{user.username}}</h3>
+                <h3>Profile picture: {{user.image}}</h3>
+            </div>
+            <div v-else>
+                
+            </div>
         </div>
 
     </div>
@@ -20,6 +26,9 @@
             return {}
             },
         methods: {
+            showProfile(){
+
+            }
         },
         computed: {
             user() {
